21b3ca7390db67cb0f45155f9499272a60209b05
Ian Stevens <iandevstevens@gmail.com>
2021-11-08 08:37:53 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+// import "prevent-pull-refresh";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://secure.ticketpros.co.za/portal/web/index2.php?page_id=bluvoucher&theme=new_widget&allow_frame=FF/c4W5p/p4wt1eYsmCcSv+ig41ZpO39REM+8M8GKsrbgxDL::e0551becc653445ae2d54f3d400784b8"
+        frameBorder="0"
+        title="Topbet"
+        height="2746px"
+      ></iframe>
     </div>
   );
 }
